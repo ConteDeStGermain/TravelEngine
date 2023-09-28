56034aa8b3709a0700a655ff523c08010c747a87
The Count <itapuc7@gmail.com>
2023-09-28 13:31:25 -0400

    removed backend folder

--- backend/gmap_tourist_scraper.d.ts
+++
@@ -1,4 +0,0 @@
-// Replace this with the actual function signature and types of your `scrapeData` function
-declare function scrapeData(city: string, country: string, type: number): Promise<any>;
-
-export { scrapeData };
---
+++ frontend/pages/mapSection.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+const MapContainer = ({ apiKey, waypoints }) => {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+    script.async = true;
+    script.defer = true;
+    script.addEventListener('load', initMap);
+
+    document.head.appendChild(script);
+
+    return () => {
+      script.removeEventListener('load', initMap);
+      document.head.removeChild(script);
+    };
+  }, [apiKey]);
+
+  const initMap = () => {
+    const directionsService = new window.google.maps.DirectionsService();
+    const directionsRenderer = new window.google.maps.DirectionsRenderer();
+    const mapOptions = {
+      center: waypoints[0],
+      zoom: 14,
+    };
+
+    const mapInstance = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+
+    directionsRenderer.setMap(mapInstance);
+
+    const request: any = {
+      origin: waypoints[0],
+      destination: waypoints[waypoints.length - 1],
+      waypoints: waypoints.slice(1, -1).map(waypoint => ({ location: waypoint })),
+      travelMode: 'WALKING',
+    };
+
+    
+    directionsService.route(request, (result, status) => {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(result);
+      }
+    });
+
+    setMap(mapInstance);
+  }
+
+  return (
+    <div className='h-[100vh] w-[100vw] bg-[#261B2C] flex justify-center items-center flex-col'>
+      <h1 className="text-white text-5xl">Your itinerary for Monday</h1>
+      <div id="map" className='h-[70vh] w-[70vw] rounded-xl z-0' />
+    </div>
+  );
+};
+
+export default MapContainer;
